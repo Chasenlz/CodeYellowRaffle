1056c2860ae7e594085e66d11dffec29800f5da7
Abrar Lone <abrarlone01@outlook.com>
2019-04-14 22:03:42 +0100

    Licenses
    Added individual file license notices

--- websites/nakedcph.js
+++ websites/nakedcph.js
@@ -1,3 +1,20 @@
+/*
+	Copyright (C) 2019 Code Yellow
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program (license.md).  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var mainBot = require('../index.js')
 var cheerio = require('cheerio');
 var userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
